8ed7de403326a36feecf8b63140c7b1392dea9bc
hwanjuson <hwanjuson@gmail.com>
2022-07-07 18:12:38 +0900

    1문 더

--- AlgorithmTest/AlgorithmTest9/Program.cs
+++ AlgorithmTest/AlgorithmTest9/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AlgorithmTest9
 {
@@ -6,7 +8,56 @@ namespace AlgorithmTest9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Solution s = new Solution();
+
+            Console.WriteLine(s.solution(new int[] { 2,1,3,2},2));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int[] priorities, int location)
+        {
+            List<int> list = new List<int>(priorities);
+            Queue<Priority> queue = new Queue<Priority>();
+            int answer = 0;
+
+            for (int i = 0; i < priorities.Length; i++)
+            {
+                queue.Enqueue(new Priority(priorities[i], i));
+            }
+
+            while (queue.Count > 0)
+            {
+                int max = list.Max();
+                Priority cur = queue.Dequeue();
+
+                while (!cur.index.Equals(max))
+                {
+                    queue.Enqueue(cur);
+
+                    cur = queue.Dequeue();
+                }
+
+                list.Remove(max);
+                answer++;
+
+                if (cur.location.Equals(location)) break;
+            }
+
+            return answer;
+        }
+
+        class Priority
+        {
+            public int index;
+            public int location;
+
+            public Priority(int index, int location)
+            {
+                this.index = index;
+                this.location = location;
+            }
         }
     }
 }
